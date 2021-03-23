b699285ddcf062af7714d34e8db5025ec3e6c80a
Avni <avni.kasikci.01@gmail.com>
2021-03-23 21:00:29 +0300

    Projects Deploy insert

---
+++ precache-manifest.4603251073d2992dd8bab05096d90a6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd3de38c75d4f827b3a",
+    "url": "/css/app.b8177a25.css"
+  },
+  {
+    "revision": "6254a5e04d04c97b170946db6e6b14b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ab134c2ec47ffb54c8",
+    "url": "/js/about.35f9e773.js"
+  },
+  {
+    "revision": "3fd3de38c75d4f827b3a",
+    "url": "/js/app.8cec47b4.js"
+  },
+  {
+    "revision": "21a68c48627b55d34145",
+    "url": "/js/chunk-vendors.fffd61f0.js"
+  },
+  {
+    "revision": "ef870e799635f68dad3beba6d9baac91",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
